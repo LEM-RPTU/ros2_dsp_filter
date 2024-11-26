6f389a13789bc0993e3ce9fca5946816dcea3f33
Dzmitry Maladzenkau <dmalad@rptu.de>
2024-11-26 14:39:29 +0100

    [DEL] fft package

--- fft/fft/parameters.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3 
-from dataclasses import dataclass
-
-@dataclass
-class ParameterSet():
-    topic: str
-    number_of_samples: int
--- fft/src/cpp_publisher_main.cpp
+++
@@ -1,9 +0,0 @@
-#include "cpp_python_package/cpp_publisher.hpp"
-
-int main(int argc, char * argv[])
-{
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<CppPublisher>());
-    rclcpp::shutdown();
-    return 0;
-}
